2612f70605ac64ddb14a946799c98f93146a1ad4
Daniel Gekeler <181665651+DanielGekeler@users.noreply.github.com>
2024-10-27 14:15:19 +0100

    Added Token interface

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,9 +1,15 @@
+interface Token {
+    text: string
+    consumed: number
+    tokenType: "Name"
+}
+
 export function Tokenize(text: string) {
     console.log(scanName(text))
 }
 
-function scanName(text: string): [string, number] {
+function scanName(text: string): Token {
     const end_chars = [" ", "[", "{"]
     const i = Math.min(...end_chars.map(s => text.indexOf(s)).filter(x => x > -1))
-    return [text.slice(0, i), i]
+    return { text: text.slice(0, i), consumed: i, tokenType: "Name" }
 }

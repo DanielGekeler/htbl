7494926a17d02f61a0974501ec437eeb4885d9c5
Daniel Gekeler <181665651+DanielGekeler@users.noreply.github.com>
2024-11-23 15:57:08 +0100

    Completed the parser

--- src/parser.ts
+++ src/parser.ts
@@ -2,7 +2,6 @@ import { Token } from "./tokenizer.js";
 
 interface ParserObject {
     TokensConsumed: number
-    // constructor(tokens: number) { this.TokensConsumed = tokens }
 }
 
 export class Selector implements ParserObject {
@@ -11,15 +10,63 @@ export class Selector implements ParserObject {
     TokensConsumed: number
 
     constructor(name: string, tags: [string, string][], tokens: number) {
-        // super(tokens)
         this.Name = name
         this.Tags = tags
         this.TokensConsumed = tokens
     }
 }
 
-export function Parse(tokens: Token[]): ParserObject | undefined {
-    return ParseSelector(tokens)
+export class TextBody implements ParserObject {
+    Content: string
+    TokensConsumed: number
+
+    constructor(content: string, tokens: number) {
+        this.Content = content
+        this.TokensConsumed = tokens
+    }
+}
+
+export class Body implements ParserObject {
+    Elements: ParserObject[]
+    TokensConsumed: number
+
+    constructor(elements: ParserObject[], tokens: number) {
+        this.Elements = elements
+        this.TokensConsumed = tokens
+    }
+}
+
+export class Element implements ParserObject {
+    Selector: ParserObject
+    Body: ParserObject
+    TokensConsumed: number
+
+    constructor(selector: ParserObject, body: ParserObject, tokens: number) {
+        this.Selector = selector
+        this.Body = body
+        this.TokensConsumed = tokens
+    }
+}
+
+export function Parse(tokens: Token[]): ParserObject[] {
+    const elements = [] as ParserObject[]
+    let i = 0
+    while (i + 1 < tokens.length) {
+        const element = ParseElement(tokens.slice(i))
+        if (!element) return elements
+        i += element.TokensConsumed
+        elements.push(element)
+    }
+    return elements
+}
+
+function ParseElement(tokens: Token[]): ParserObject | undefined {
+    const selector = ParseSelector(tokens)
+    if (!selector) return
+    const body = ParseBody(tokens.slice(selector.TokensConsumed))
+    if (!body) return
+    return new Element(selector, body,
+        selector.TokensConsumed + body.TokensConsumed)
 }
 
 function ParseSelector(tokens: Token[]): ParserObject | undefined {
@@ -30,20 +77,18 @@ function ParseSelector(tokens: Token[]): ParserObject | undefined {
         return new Selector(name.text, [[tokens[1].text, tokens[2].text]], 3)
     }
     return new Selector(name.text, [], 1)
+}
 
-    // const tags: { [key: string]: string } = {}
-    /*const tags = [] as [string, string][]
-    let i = 1 // token count#
-    let last_key = ""
-    for (const token of tokens.slice(1)) {
-        if (token.tokenType == "AttrName") {
-            last_key = token.text
-        } else if (token.tokenType == "AttrValue") {
-            // tags[last_key] = token.text
-            tags.push([last_key, token.text])
-        } else break
-        i++
+function ParseBody(tokens: Token[]): ParserObject | undefined {
+    if (tokens.length > 0 && tokens[0].tokenType == "TextBody")
+        return new TextBody(tokens[0].text, 1)
+
+    if (tokens.length > 0 && tokens[0].tokenType == "BodyBegin") {
+        let i = 1
+        const elements = Parse(tokens.slice(i))
+        i += elements.reduce((x, y) => x + y.TokensConsumed, 0)
+        if (tokens[i].tokenType != "BodyEnd") return
+        return new Body(elements, i + 1)
     }
-
-    return new Selector(name.text, tags, i)*/
+    return
 }
